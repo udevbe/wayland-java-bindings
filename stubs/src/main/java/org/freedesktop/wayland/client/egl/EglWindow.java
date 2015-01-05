c0b74b275d477d7215012c5bba36c90b44e00d08
Erik De Rijcke <derijcke.erik@gmail.com>
2015-01-05 21:46:13 +0100

    convert wayland egl to jna

--- stubs/src/main/java/org/freedesktop/wayland/client/egl/EglWindow.java
+++ stubs/src/main/java/org/freedesktop/wayland/client/egl/EglWindow.java
@@ -1,113 +1,119 @@
 package org.freedesktop.wayland.client.egl;
 
-import org.freedesktop.wayland.HasPointer;
+import com.sun.jna.ptr.IntByReference;
+import org.freedesktop.wayland.HasNative;
 import org.freedesktop.wayland.client.Proxy;
+import org.freedesktop.wayland.client.egl.jna.WaylandEglLibrary;
+import org.freedesktop.wayland.client.egl.jna.wl_egl_window;
 import org.freedesktop.wayland.util.ObjectCache;
 
-public class EglWindow implements HasPointer {
+public class EglWindow implements HasNative<wl_egl_window> {
 
-  public static final class Size{
+    public static final class Size {
 
-    private final int width;
-    private final int height;
+        private final int width;
+        private final int height;
 
-    Size(final int width,
-         final int height) {
-      this.width = width;
-      this.height = height;
+        Size(final int width,
+             final int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int getWidth() {
+            return this.width;
+        }
+
+        public int getHeight() {
+            return this.height;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            final Size size = (Size) o;
+
+            return this.height == size.height && this.width == size.width;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = this.width;
+            result = 31 * result + this.height;
+            return result;
+        }
     }
 
-    public int getWidth() {
-      return width;
+    private final wl_egl_window pointer;
+
+    public static EglWindow create(final Proxy<?> wlSurfaceProxy,
+                                   final int width,
+                                   final int height) {
+        return new EglWindow(WaylandEglLibrary.INSTANCE.wl_egl_window_create(wlSurfaceProxy.getNative()
+                                                                                           .getPointer(),
+                                                                             width,
+                                                                             height));
     }
 
-    public int getHeight() {
-      return height;
+    protected EglWindow(final wl_egl_window pointer) {
+        this.pointer = pointer;
+        ObjectCache.store(getNative().getPointer(),
+                          this);
+    }
+
+    public void destroy() {
+        WaylandEglLibrary.INSTANCE.wl_egl_window_destroy(getNative());
+        ObjectCache.remove(getNative().getPointer());
+    }
+
+    public void resize(final int width,
+                       final int height,
+                       final int dx,
+                       final int dy) {
+        WaylandEglLibrary.INSTANCE.wl_egl_window_resize(getNative(),
+                                                        width,
+                                                        height,
+                                                        dx,
+                                                        dy);
+    }
+
+    public Size getAttachedSize() {
+        final IntByReference x = new IntByReference();
+        final IntByReference y = new IntByReference();
+        WaylandEglLibrary.INSTANCE.wl_egl_window_get_attached_size(getNative(),
+                                                                   x,
+                                                                   y);
+        return new Size(x.getValue(),
+                        y.getValue());
+    }
+
+    @Override
+    public wl_egl_window getNative() {
+        return this.pointer;
     }
 
     @Override
     public boolean equals(final Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof EglWindow)) {
+            return false;
+        }
 
-      final Size size = (Size) o;
+        final EglWindow eglWindow = (EglWindow) o;
 
-      return height == size.height && width == size.width;
+        return this.pointer.equals(eglWindow.pointer);
     }
 
     @Override
     public int hashCode() {
-      int result = width;
-      result = 31 * result + height;
-      return result;
+        return this.pointer.hashCode();
     }
-  }
-
-  private long pointer;
-
-  public static EglWindow create(Proxy<?> wlSurfaceProxy,
-                                 int width,
-                                 int height){
-    return new EglWindow(WlEglJNI.createEglWindow(wlSurfaceProxy.getPointer(),
-                                                  width,
-                                                  height));
-  }
-
-  protected EglWindow(final long pointer) {
-    this.pointer = pointer;
-    ObjectCache.store(getPointer(),
-                      this);
-  }
-
-  public void destroy(){
-    WlEglJNI.destroyEglWindow(getPointer());
-    ObjectCache.remove(getPointer());
-  }
-
-  public void resize(int width,
-                     int height,
-                     int dx,
-                     int dy){
-    WlEglJNI.resize(getPointer(),
-                    width,
-                    height,
-                    dx,
-                    dy);
-  }
-
-  public Size getAttachedSize(){
-    final int[] size = new int[2];
-    WlEglJNI.getAttachedSize(getPointer(),
-                             size);
-    return new Size(size[0],
-                    size[1]);
-  }
-
-  @Override
-  public long getPointer() {
-    return this.pointer;
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof EglWindow)) {
-      return false;
-    }
-
-    final EglWindow eglWindow = (EglWindow) o;
-
-    return pointer == eglWindow.pointer;
-  }
-
-  @Override
-  public int hashCode() {
-    return (int) pointer;
-  }
 }
--- stubs/src/main/java/org/freedesktop/wayland/client/jna/wl_event_queue.java
+++ stubs/src/main/java/org/freedesktop/wayland/client/jna/wl_event_queue.java
@@ -4,10 +4,11 @@ import com.sun.jna.Pointer;
 import com.sun.jna.PointerType;
 
 public class wl_event_queue extends PointerType {
-  public wl_event_queue(Pointer address) {
-    super(address);
-  }
-  public wl_event_queue() {
-    super();
-  }
+    public wl_event_queue(final Pointer address) {
+        super(address);
+    }
+
+    public wl_event_queue() {
+        super();
+    }
 }
