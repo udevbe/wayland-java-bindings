dfa29b06687826fa9e9e68d8e7476f0d453ee0f2
Erik De Rijcke <derijcke.erik@gmail.com>
2015-01-25 20:35:03 +0100

    update copyright headers

--- examples/src/main/java/examples/SimpleShm.java
+++ examples/src/main/java/examples/SimpleShm.java
@@ -1,15 +1,28 @@
+//Copyright 2015 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 package examples;
 
 public class SimpleShm {
 
-    public static void main(String args[]) {
+    public static void main(final String[] args) {
 
         Native.loadLibrary("wayland-java-util");
 
-        Display display = new Display();
-        Window window = new Window(display,
-                                   250,
-                                   250);
+        final Display display = new Display();
+        final Window window = new Window(display,
+                                         250,
+                                         250);
         window.redraw(0);
 
         try {
@@ -18,7 +31,7 @@ public class SimpleShm {
                        .dispatch();
             }
         }
-        catch (Exception e) {
+        catch (final Exception e) {
             window.destroy();
             display.destroy();
             throw new RuntimeException(e);
