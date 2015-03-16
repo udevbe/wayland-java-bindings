6513d615f6ee31f7c5dc30665f3d0dcbed4bd36d
Erik De Rijcke <Erik.De.Rijcke@prodatamobility.com>
2015-03-16 17:10:54 +0100

    provide better buffer management

---
+++ examples/src/main/java/examples/Buffer.java
@@ -0,0 +1,39 @@
+package examples;
+
+import org.freedesktop.wayland.client.WlBufferEvents;
+import org.freedesktop.wayland.client.WlBufferProxy;
+
+import java.nio.ByteBuffer;
+
+
+public class Buffer implements WlBufferEvents {
+
+    private final BufferPool bufferPool;
+    private final ByteBuffer byteBuffer;
+    private final int width;
+    private final int height;
+
+    public Buffer(final BufferPool bufferPool, final ByteBuffer byteBuffer, final int width, final int height) {
+        this.bufferPool = bufferPool;
+        this.byteBuffer = byteBuffer;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void release(final WlBufferProxy emitter) {
+        bufferPool.queueBuffer(emitter);
+    }
+
+    public ByteBuffer getByteBuffer() {
+        return byteBuffer;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
