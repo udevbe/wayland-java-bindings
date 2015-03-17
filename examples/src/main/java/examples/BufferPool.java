67da916119e02a3b16c84ac9c45f187a59aa5731
zubnix <DeRijcke.Erik@gmail.com>
2015-03-17 14:51:20 +0100

    don't allow bufferpool usage when it is destroyed

--- examples/src/main/java/examples/BufferPool.java
+++ examples/src/main/java/examples/BufferPool.java
@@ -11,26 +11,35 @@ public class BufferPool implements WlShmPoolEvents{
     private LinkedList<WlBufferProxy> bufferQueue = new LinkedList<WlBufferProxy>();
     private boolean destroyed;
 
-
-    public BufferPool() {
-    }
-
     public void queueBuffer(WlBufferProxy buffer){
         if(destroyed){
-            buffer.destroy();
-        }else {
-            this.bufferQueue.add(buffer);
+            throw new IllegalStateException("Pool destroyed");
         }
+
+        this.bufferQueue.add(buffer);
     }
 
     public WlBufferProxy popBuffer(){
+        if(destroyed){
+            throw new IllegalStateException("Pool destroyed");
+        }
+
         return bufferQueue.pop();
     }
 
     public void destroy(){
+        if(destroyed){
+            throw new IllegalStateException("Pool destroyed");
+        }
+
         for (WlBufferProxy wlBufferProxy : bufferQueue) {
             wlBufferProxy.destroy();
         }
+        bufferQueue.clear();
         this.destroyed = true;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
     }
 }
