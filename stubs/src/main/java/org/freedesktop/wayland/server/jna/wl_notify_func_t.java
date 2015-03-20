151fe9ea3e0d9584679752174427703525e9bf00
Erik De Rijcke <derijcke.erik@gmail.com>
2015-03-20 22:20:03 +0100

    use static callback functions to reduce memory consumption

--- stubs/src/main/java/org/freedesktop/wayland/server/jna/wl_notify_func_t.java
+++ stubs/src/main/java/org/freedesktop/wayland/server/jna/wl_notify_func_t.java
@@ -14,7 +14,8 @@
 package org.freedesktop.wayland.server.jna;
 
 import com.sun.jna.Callback;
+import com.sun.jna.Pointer;
 
 public interface wl_notify_func_t extends Callback {
-    void apply();
+    void apply(Pointer listenerPointer);
 }
